22c6e22ff52347073d02a8c604c4d5905e2436df
Rumman <rummanhase@gmail.com>
2024-06-30 20:12:44 +0530

    added routes for users

---
+++ server/Routers/userRoute.js
@@ -0,0 +1,11 @@
+const express  = require("express");
+const {registerUser , loginUser , findUser , getUsers} = require("../Controllers/userController.js")
+
+const router = express.Router();
+
+router.post("/register" , registerUser)
+router.post("/login" , loginUser)
+router.get("/user/:userId" , findUser);
+router.get("/getUsers" , getUsers)
+
+module.exports = router; 
